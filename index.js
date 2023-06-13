7a937dd77b819d2b434154225d23d4b8def25769
Martin Tomov <devsonspree@gmail.com>
2023-06-13 19:24:15 +0800

    index.js replace

--- index.js
+++ index.js
@@ -7,30 +7,10 @@ async function main() {
 }
 
 const express = require("express");
-const bodyParser = require("body-parser");
-const cors = require("cors");
 
 // App constants
 const port = process.env.PORT || 3000;
-const apiPrefix = "/api";
-
-// Create the Express app & setup middlewares
 const app = express();
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
-app.use(cors({ origin: /http:\/\/(127(\.\d){3}|localhost)/ }));
-app.options("*", cors());
-
-// Configure routes
-const router = express.Router();
-
-// Hello World for index page
-app.get("/", function (req, res) {
-  main();
-});
-
-// Add 'api` prefix to all routes
-app.use(apiPrefix, router);
 
 // Start the server
 app.listen(port, () => {
