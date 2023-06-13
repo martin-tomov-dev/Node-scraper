1a3a2dc5a6341736f877143722d1633804775590
Martin Tomov <devsonspree@gmail.com>
2023-06-13 17:44:42 +0800

    replace index.js file

--- index.js
+++ index.js
@@ -6,4 +6,33 @@ async function main() {
   scraper(random_number);
 }
 
-main();
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+
+// App constants
+const port = process.env.PORT || 3000;
+const apiPrefix = "/api";
+
+// Create the Express app & setup middlewares
+const app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(cors({ origin: /http:\/\/(127(\.\d){3}|localhost)/ }));
+app.options("*", cors());
+
+// Configure routes
+const router = express.Router();
+
+// Hello World for index page
+app.get("/", function (req, res) {
+  main();
+});
+
+// Add 'api` prefix to all routes
+app.use(apiPrefix, router);
+
+// Start the server
+app.listen(port, () => {
+  main();
+});
