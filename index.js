29f566f801d951ecca00548215a6874ac5273a03
Martin Tomov <devsonspree@gmail.com>
2023-06-13 20:11:54 +0800

    update async function

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const scraper = require("./scrapers/scraper");
 
-async function main() {
+function main() {
   var random_number = Math.floor(Math.random() * 3) + 1;
   console.log(random_number);
   scraper(random_number);
