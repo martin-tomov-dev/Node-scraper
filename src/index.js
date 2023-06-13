37985f70d46fdd41a7e21f75a5eaa666e323e495
Martin Tomov <devsonspree@gmail.com>
2023-06-13 16:52:38 +0800

    init node project

--- src/index.js
+++
@@ -1,9 +0,0 @@
-const scraper = require("../scrapers/scraper");
-
-async function main() {
-  var random_number = Math.floor(Math.random() * 3) + 1;
-  console.log(random_number);
-  scraper(random_number);
-}
-
-main();
